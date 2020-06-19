a92aedd9fc57e490be3fed36d5119914119e0ee6
tushar kamra <tushar.kamra25@gmail.com>
2020-06-19 16:01:03 +0530

    removed

--- delete_item.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include "connect.php";
-    $id=$_POST['item_id'];
-	header('location:vendor_dashboard.php');
-	$sql = "delete from catalog where item_id='$id'";
-	$result1  = $conn->query($sql);
-?>
